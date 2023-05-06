9feb88aa4d0b83a3f4af5ffc78942540c60f7efc
Arthur Oliveira <afuzarodeoliveira@gmail.com>
2023-05-06 10:40:58 -0300

    Fixing repo name of artificat registry

--- gcp-main.tf
+++ gcp-main.tf
@@ -1,13 +1,13 @@
 resource "google_artifact_registry_repository" "spotmusic-backend-repo" {
   location = var.region
-  repository_id = "spotMusic - Backend"
+  repository_id = "spotMusic-Backend"
   description = "Imagens Docker do backend da spot music"
   format = "DOCKER"
 }
 
-resource "google_artifact_registry_repository" "spotmusic-Frontend-repo" {
+resource "google_artifact_registry_repository" "spotmusic-frontend-repo" {
   location = var.region
-  repository_id = "spotMusic - Frontend"
+  repository_id = "spotMusic-Frontend"
   description = "Imagens Docker do Frontend da spot music"
   format = "DOCKER"
 }
