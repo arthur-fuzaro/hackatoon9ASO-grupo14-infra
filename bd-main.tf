03a237b34afa8567b2755f645d8eb5231b6c5fc1
Arthur Oliveira <afuzarodeoliveira@gmail.com>
2023-05-06 14:15:52 -0300

    Removed database script init

--- bd-main.tf
+++ bd-main.tf
@@ -19,28 +19,4 @@ resource "google_sql_user" "playlist_user" {
   password = "$p@tmusic"
   instance = google_sql_database_instance.playlist-sql.name
   host     = "%"
-  port     = 3306
-  database = "playlist"
-}
-
-resource "google_storage_bucket_object" "sql_script" {
-  name   = "Playlist.sql"
-  bucket = "playlist_bucket_grupo14"
-
-  content = file("Playlist.sql")
-}
-
-
-resource "null_resource" "execute_script" {
-  depends_on = [google_sql_user.playlist_user]
-
-  provisioner "local-exec" {
-    command = "PGPASSWORD=${MYSQL_PWD} psql -f Playlist.sql -p ${MYSQL_PORT} -U ${MYSQL_USERNAME} ${MYSQL_DATABASE}"
-    environment = {
-      MYSQL_PWD = google_sql_user.playlist_user.password
-      MYSQL_PORT = google_sql_user.playlist_user.port
-      MYSQL_USERNAME = google_sql_user.playlist_user.name
-      MYSQL_DATABASE = google_sql_user.playlist_user.database
-    }
-  }
 }
